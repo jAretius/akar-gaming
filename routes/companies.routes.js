6ce45829cf47bd88014ce1f93c7b260e7f093be8
AitorMartinez <aitormartinezretamosa>
2020-09-21 13:13:37 +0200

    CRUD companies & games

--- routes/companies.routes.js
+++ routes/companies.routes.js
@@ -12,6 +12,41 @@ router.get("/", (req, res, next) => {
         .catch(err => next(err))
 })
 
+//New Company
+router.get("/new", (req, res, next) =>  res.render("companies/new"))
+
+router.post("/new", (req, res, next) => {
+    const { name, logo, description, website} = req.body
+
+    Company.create({ name, logo, description, website })
+        .then(() => res.redirect("/companies/new"))
+        .catch(err => next(new Error(err)))
+})
+
+//Company Update
+router.get("/edit", (req, res, next) => {
+    
+    Company.findById(req.query.id)
+        .then(companyDetails => res.render("companies/update", companyDetails))
+        .catch(err => next(new Error(err)))
+})
+
+router.post("/edit", (req, res, next) => {
+    const { name, logo, description, website } = req.body
+
+    Company.findByIdAndUpdate(req.query.id, { name, logo, description, website })
+        .then(() => res.redirect("/companies"))
+        .catch(err => next(new Error(err)))
+})
+
+//Company Delete
+router.get("/delete", (req, res, next) => {
+
+    Company.findByIdAndDelete(req.query.id)
+        .then(() => res.redirect("/companies"))
+        .catch(err => next(new Error(err)))
+})
+
 //Company details
 router.get("/:companyId", (req, res, next) => {
     const id = req.params.companyId
